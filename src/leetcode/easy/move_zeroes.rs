5085c055c908ce6901c24e9038e3a40be8437e56
Natechawin <natechawin@gmail.com>
2021-03-20 00:47:58 +0700

    Refactor to use data driven test

--- src/leetcode/easy/move_zeroes.rs
+++ src/leetcode/easy/move_zeroes.rs
@@ -4,12 +4,10 @@ struct Solution {}
 impl Solution {
     pub fn move_zeroes(nums: &mut Vec<i32>) {
         let mut idx = 0;
-        
         while idx < nums.len() {
             if nums[idx] == 0 {
                 nums.remove(idx);
                 nums.append(&mut vec![0]);
-                
                 if is_the_rest_only_zero(&nums, idx) {
                     break;
                 }
@@ -33,21 +31,15 @@ fn is_the_rest_only_zero(nums: &Vec<i32>, current_idx: usize) -> bool {
 #[cfg(test)]
 pub mod solution_test {
     use super::*;
+    use rstest::rstest;
 
-    #[test]
-    fn move_zeroes_when_given_vector_of_interger_should_move_all_zero_to_the_end_of_vector() {
-        let mut nums:Vec<i32> = vec![0,1,0,3,12];
+    #[rstest(input, expected, 
+        case(&mut vec![0,1,0,3,12], &mut vec![1,3,12,0,0]),
+        case(&mut vec![0, 0, 1], &mut vec![1, 0, 0]),
+    )]
+    fn move_zeroes_when_given_vector_of_interger_should_move_all_zero_to_the_end_of_vector(input: &mut Vec<i32>, expected: &mut Vec<i32>,) {
+        Solution::move_zeroes(input);
 
-        Solution::move_zeroes(&mut nums);
-
-        assert_eq!(nums, vec![1,3,12,0,0]);
-    }
-    #[test]
-    fn move_zeroes_when_given_vector_of_interger_should_move_all_zero_to_the_end_of_vector_2() {
-        let mut nums:Vec<i32> = vec![0,0,1];
-
-        Solution::move_zeroes(&mut nums);
-
-        assert_eq!(nums, vec![1,0,0]);
+        assert_eq!(input, expected);
     }
 }
