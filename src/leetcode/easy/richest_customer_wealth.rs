b7251f91eb30e757ef5bb2c9c44eb11508a66a82
Natechawin <natechawin@gmail.com>
2021-03-23 23:44:25 +0700

    Add test cases for richest_customer_wealth

--- src/leetcode/easy/richest_customer_wealth.rs
+++ src/leetcode/easy/richest_customer_wealth.rs
@@ -2,14 +2,35 @@ struct Solution {}
 
 impl Solution {
     pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
-        accounts.iter().map(|account| {
-            account.iter().sum()
-        }).fold(0, |a,b|{
-            if a > b {
-                a
-            } else {
-                b
-            }
-        })
+        accounts.iter().map(|account| account.iter().sum()).fold(
+            0,
+            |a, b| {
+                if a > b {
+                    a
+                } else {
+                    b
+                }
+            },
+        )
+    }
+}
+
+#[cfg(test)]
+pub mod solution_test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest(input, expected,
+    case(vec![vec![1,2,3],vec![3,2,1]], 6),
+    case(vec![vec![1,5],vec![7,3],vec![3,5]], 10),
+    case(vec![vec![2,8,7],vec![7,1,3],vec![1,9,5]], 17),
+    )]
+    fn maximum_wealth_when_given_vector_of_customer_accounts_should_return_the_maximum_wealth(
+        input: Vec<Vec<i32>>,
+        expected: i32,
+    ) {
+        let result = Solution::maximum_wealth(input);
+
+        assert_eq!(result, expected);
     }
 }
