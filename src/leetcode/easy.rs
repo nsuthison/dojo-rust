870b668224f4456fda4dd191cb49e447255bd0ff
Natechawin <natechawin@gmail.com>
2021-03-25 00:33:44 +0700

    Finish solution for first_unique_character_in_a_string question

--- src/leetcode/easy.rs
+++ src/leetcode/easy.rs
@@ -1,2 +1,3 @@
+pub mod first_unique_character_in_a_string;
 pub mod move_zeroes;
 pub mod richest_customer_wealth;
---
+++ src/leetcode/easy/first_unique_character_in_a_string.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+struct Solution;
+
+impl Solution {
+    pub fn first_uniq_char(s: String) -> i32 {
+        let mut alphabet_is_unique: HashMap<char, bool> = HashMap::new();
+
+        for (_, c) in s.chars().enumerate() {
+            if alphabet_is_unique.contains_key(&c) {
+                alphabet_is_unique.insert(c, false);
+            } else {
+                alphabet_is_unique.insert(c, true);
+            }
+        }
+
+        for (idx, c) in s.chars().enumerate() {
+            if alphabet_is_unique[&c] {
+                return idx as i32;
+            }
+        }
+
+        -1
+    }
+}
+
+#[cfg(test)]
+pub mod solution_test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest(input, expected, case("leetcode", 0), case("loveleetcode", 2))]
+    fn first_uniq_char_should_return_first_idx_of_unique_char_when_given_string_with_unique_char(
+        input: &str,
+        expected: i32,
+    ) {
+        let result = Solution::first_uniq_char(String::from(input));
+
+        assert_eq!(result, expected);
+    }
+}
