2312a5be21cd197466d3781c73e6cf8295f17474
Natechawin <natechawin@gmail.com>
2021-03-25 01:35:09 +0700

    Add allow map_entry to avoid clippy warning

--- src/leetcode/easy/first_unique_character_in_a_string.rs
+++ src/leetcode/easy/first_unique_character_in_a_string.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 struct Solution;
 
 /// Question: https://leetcode.com/problems/first-unique-character-in-a-string/solution/
+#[allow(clippy::map_entry)] // In this case map_entry is not make sense when we want to update false if entry is exist.
 impl Solution {
     pub fn first_uniq_char(s: String) -> i32 {
         let mut alphabet_is_unique: HashMap<char, bool> = HashMap::new();
