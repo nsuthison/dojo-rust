67d23e9eb15f2afe35eb7af5b1d73174d5b8ef46
Natechawin <natechawin@gmail.com>
2021-03-26 23:58:58 +0700

    Try to find the way to filter char from string

---
+++ src/leetcode/easy/valid_palindromes.rs
@@ -0,0 +1,13 @@
+struct Solution;
+
+/// Question: https://leetcode.com/problems/valid-palindrome/
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        true
+    }
+
+    pub fn remove_all_non_alphanumeric(s: &str) -> &str {
+        let a = s.chars().filter(|x| x.is_alphanumeric());
+        "something"
+    }
+}
