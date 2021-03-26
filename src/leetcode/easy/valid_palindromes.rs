95ec76f245f8a7a2f0bc9415b8a16503d1e2f19e
Natechawin <natechawin@gmail.com>
2021-03-27 00:03:31 +0700

    Comment out whole palindrome question

--- src/leetcode/easy/valid_palindromes.rs
+++ src/leetcode/easy/valid_palindromes.rs
@@ -2,12 +2,12 @@ struct Solution;
 
 /// Question: https://leetcode.com/problems/valid-palindrome/
 impl Solution {
-    pub fn is_palindrome(s: String) -> bool {
-        true
-    }
-
-    pub fn remove_all_non_alphanumeric(s: &str) -> &str {
-        let a = s.chars().filter(|x| x.is_alphanumeric());
-        "something"
-    }
+    // pub fn is_palindrome(s: String) -> bool {
+    //     true
+    // }
+    //
+    // pub fn remove_all_non_alphanumeric(s: &str) -> &str {
+    //     let a = s.chars().filter(|x| x.is_alphanumeric()).collect();
+    //     "something"
+    // }
 }
