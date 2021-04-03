e2d148d35edfae0156603dcb612bf768774e9b11
Natechawin <natechawin@gmail.com>
2021-04-03 23:48:32 +0700

    add: matrix check boundary

--- src/leetcode/medium/number_of_provinces/solution.rs
+++ src/leetcode/medium/number_of_provinces/solution.rs
@@ -1,17 +1,28 @@
 use crate::utils::matrix::coordinate::Coordinate;
 
 /// Question: https://leetcode.com/problems/number-of-provinces/
-// impl Solution {
-//     pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
-//         0
-//     }
-// }
-//
+impl Solution {
+    // pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
+    //     let mut number_of_island = 0;
+    //     0
+    // }
+}
+
 // fn find_number_of_provinces(is_connected: &mut Vec<&mut Vec<i32>>) -> i32 {
 //     0
 // }
 
-fn update(is_connected: &mut Vec<Vec<i32>>, at: Coordinate, by: i32) -> &mut Vec<Vec<i32>> {
+fn check_province(coordinate: &Coordinate, is_connected: &mut Vec<Vec<i32>>) {
+    if is_connected[coordinate.column as usize][coordinate.row as usize] == 1 {
+        update(is_connected, coordinate, 0);
+    }
+}
+
+fn update<'a>(
+    is_connected: &'a mut Vec<Vec<i32>>,
+    at: &'a Coordinate,
+    by: i32,
+) -> &'a mut Vec<Vec<i32>> {
     is_connected[at.row as usize][at.column as usize] = by;
     is_connected
 }
@@ -30,7 +41,7 @@ pub mod solution_test {
     expected,
     case(vec ! [vec ! [1, 1, 1], vec ! [1, 1, 1]], Coordinate { row: 0, column: 1}, 0, vec ! [vec ! [1, 0, 1], vec ! [1, 1, 1]]),
     )]
-    fn update_is_connected_should_return_same_is_connected_with_data_change_in_specific_coordinate_by_specific_value(
+    fn update_is_connected_should_return_is_connected_with_data_change_in_specific_coordinate_by_specific_value(
         input: Vec<Vec<i32>>,
         at: Coordinate,
         by: i32,
@@ -39,6 +50,6 @@ pub mod solution_test {
         let input = &mut input.to_owned();
         let expected = &mut expected.to_owned();
 
-        assert_eq!(update(input, at, by), expected);
+        assert_eq!(update(input, &at, by), expected);
     }
 }
--- src/utils/matrix.rs
+++ src/utils/matrix.rs
@@ -1,2 +1,56 @@
+use crate::utils::matrix::coordinate::Coordinate;
+
 pub mod coordinate;
 pub mod direction;
+
+pub struct Matrix<'a, T> {
+    data: &'a [Vec<T>],
+}
+
+impl<T> Matrix<'_, T> {
+    fn new(matrix: &[Vec<T>]) -> Matrix<T> {
+        Matrix { data: matrix }
+    }
+
+    fn is_in_boundary(&self, cell: &Coordinate) -> bool {
+        let is_row_inbound = || cell.row >= 0 && cell.row < self.data.len() as i32;
+        let is_column_inbound =
+            || cell.column >= 0 && cell.column < self.data[cell.row as usize].len() as i32;
+
+        is_row_inbound() && is_column_inbound()
+    }
+}
+
+#[cfg(test)]
+pub mod matrix_test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest(
+    input,
+    coordinate,
+    case(vec ! [vec ! [1, 1, 1], vec ! [1, 1, 1]], Coordinate { row: 0, column: 1}),
+    )]
+    fn is_in_boundary_should_return_true_when_given_coordinate_is_in_the_boundary(
+        input: Vec<Vec<i32>>,
+        coordinate: Coordinate,
+    ) {
+        let matrix = Matrix::new(&input);
+
+        assert_eq!(matrix.is_in_boundary(&coordinate), true);
+    }
+
+    #[rstest(
+    input,
+    coordinate,
+    case(vec ! [vec ! [1, 1, 1], vec ! [1, 1, 1]], Coordinate { row: 5, column: 5}),
+    )]
+    fn is_in_boundary_should_return_face_when_given_coordinate_is_outside_the_boundary(
+        input: Vec<Vec<i32>>,
+        coordinate: Coordinate,
+    ) {
+        let matrix = Matrix::new(&input);
+
+        assert_eq!(matrix.is_in_boundary(&coordinate), false);
+    }
+}
