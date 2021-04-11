96c7355167950d29ebe795e15047c146558824b5
Natechawin <natechawin@gmail.com>
2021-04-11 14:52:45 +0700

    chore: add commenting cantained to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-#![allow(dead_code)]
+//! # dojo-rust
+//!
+//! `dojo-rust` is a small dojo for practicing Rust programming language.
 
+#![allow(dead_code)] // Allow dead code because most of the lib here is to solve algorithm problems which no use in root.
 mod leetcode;
 mod utils;
