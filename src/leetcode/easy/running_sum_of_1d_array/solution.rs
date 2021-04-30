2c72248a4b52e2958f8e0c24a26de43244cc05be
Natechawin <natechawin@gmail.com>
2021-05-01 00:08:39 +0700

    test: add test for running_sum_of_1d_array

--- src/leetcode/easy/running_sum_of_1d_array/solution.rs
+++ src/leetcode/easy/running_sum_of_1d_array/solution.rs
@@ -14,3 +14,23 @@ impl Solution {
 }
 
 struct Solution;
+
+#[cfg(test)]
+pub mod solution_test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest(
+    input,
+    expected,
+    case(vec![1,2,3,4], vec![1,3,6,10]),
+    case(vec![1,1,1,1,1], vec![1,2,3,4,5]),
+    case(vec![3,1,2,10,1], vec![3,4,6,16,17])
+    )]
+    fn running_sum_should_return_vector_contain_summation_of_running_number_when_given_non_empty_vector(
+        input: Vec<i32>,
+        expected: Vec<i32>,
+    ) {
+        assert_eq!(Solution::running_sum(input), expected);
+    }
+}
