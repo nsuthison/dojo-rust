177534be16ff9b2f3b7fd56d181d1531dc753f2f
Natechawin <natechawin@gmail.com>
2021-05-01 00:16:42 +0700

    refactor: create first acc explicitly

--- src/leetcode/easy/running_sum_of_1d_array/solution.rs
+++ src/leetcode/easy/running_sum_of_1d_array/solution.rs
@@ -1,12 +1,10 @@
 /// Question: https://leetcode.com/problems/running-sum-of-1d-array/
 impl Solution {
     pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
-        nums.into_iter().fold(vec![], |mut acc, x| {
-            if acc.is_empty() {
-                acc.push(x);
-            } else {
-                acc.push(x + acc.last().unwrap_or(&0));
-            }
+        let acc_with_first_elem = vec![*(nums.first().unwrap_or(&0))];
+
+        nums.into_iter().skip(1).fold(acc_with_first_elem, |mut acc, x| {
+            acc.push(x + acc.last().unwrap_or(&0));
 
             acc
         })
