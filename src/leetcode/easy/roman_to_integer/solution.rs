633ee58fb887c020af3df1c88dfc2c6e04975e4b
Natechawin <natechawin@gmail.com>
2021-11-20 05:27:24 +0700

    make it work aka ugly version
    too sleepy now will refactor tmr

--- src/leetcode/easy/roman_to_integer/solution.rs
+++ src/leetcode/easy/roman_to_integer/solution.rs
@@ -18,7 +18,36 @@ static ROMAN_NUMBER_VALUE: Lazy<HashMap<char, i32>> = Lazy::new(|| {
 /// Question: https://leetcode.com/problems/roman-to-integer/
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
-        todo!()
+        let mut previous_roman_number = "".to_owned();
+        let mut result = 0;
+        let size = s.chars().count();
+
+        if size == 1 {
+            return get_dec_value_from_roman_number(&s).unwrap();
+        }
+
+        for (idx, roman_number_symbol) in s.chars().enumerate() {
+            if previous_roman_number == "" {
+                previous_roman_number = format!("{}", roman_number_symbol);
+                continue;
+            }
+
+            let to_check = format!("{}{}", previous_roman_number, roman_number_symbol);
+
+            if is_legit_roman_number(&to_check) {
+                if idx < size - 1 {
+                    previous_roman_number = format!("{}", to_check);
+                    continue;
+                } else {
+                    result += get_dec_value_from_roman_number(&to_check).unwrap();
+                }
+            } else {
+                result += get_dec_value_from_roman_number(&previous_roman_number).unwrap();
+                previous_roman_number = format!("{}", roman_number_symbol);
+            }
+        }
+
+        result
     }
 }
 
@@ -75,11 +104,7 @@ fn is_first_roman_symbol_lower_than_the_second_one(roman_number: &str) -> bool {
     let first = try_get_dec_value_from_roman_nth(roman_number, 0);
     let second = try_get_dec_value_from_roman_nth(roman_number, 1);
 
-    if first <= second {
-        true
-    } else {
-        false
-    }
+    first <= second
 }
 
 fn are_all_three_roman_symbol_the_same(roman_number: &str) -> bool {
@@ -87,11 +112,7 @@ fn are_all_three_roman_symbol_the_same(roman_number: &str) -> bool {
     let second = try_get_dec_value_from_roman_nth(roman_number, 1);
     let third = try_get_dec_value_from_roman_nth(roman_number, 2);
 
-    if (first == second) && (first == third) {
-        true
-    } else {
-        false
-    }
+    (first == second) && (first == third)
 }
 
 #[cfg(test)]
@@ -144,5 +165,14 @@ pub mod solution_test {
         assert!(is_legit_roman_number("III"));
         assert!(is_legit_roman_number("CCC"));
         assert!(is_legit_roman_number("XXX"));
+    }
+
+    #[test]
+    fn roman_to_int_should_return_decimal_value_when_given_legit_roman_number() {
+        // assert_eq!(1, Solution::roman_to_int("I".to_owned()));
+        // assert_eq!(4, Solution::roman_to_int("IV".to_owned()));
+        // assert_eq!(9, Solution::roman_to_int("IX".to_owned()));
+        assert_eq!(58, Solution::roman_to_int("LVIII".to_owned()));
+        assert_eq!(1994, Solution::roman_to_int("MCMXCIV".to_owned()));
     }
 }
